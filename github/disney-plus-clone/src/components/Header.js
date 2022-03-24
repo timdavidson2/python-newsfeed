de317c4f4f9239a78c5a5a8b2c0f9ef169a7a860
Tim Davidson <timdavidson2@gmail.com>
2022-03-24 11:49:23 -0400

    new commit

---
+++ github/disney-plus-clone/src/components/Header.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src='/images/logo.svg' />
+      <NavMenu>
+        <a>
+          <img src='/images/home-icon.svg' />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src='/images/search-icon.svg' />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src='/images/watchlist-icon.svg' />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src='/images/original-icon.svg' />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src='/images/movie-icon.svg' />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src='/images/series-icon.svg' />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src='/images/navImg.jpg' />
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 12px;
+      letter-spacing: 1.5px;
+
+      &:after {
+        content: '';
+        height: 2px;
+        background: white;
+      }
+    }
+  }
+`;
+const UserImg = styled.img`
+  height: 48px;
+  width: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
